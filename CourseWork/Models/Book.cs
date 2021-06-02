708cefd31bdb0e5c79b9a9e58e14f6b009c0bd6a
max <mail.max.khomenko@gmail.com>
2021-06-02 19:20:41 +0300

    Made name field required

--- CourseWork/Models/Book.cs
+++ CourseWork/Models/Book.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using Avalonia.Media.Imaging;
 
 namespace CourseWork.Models
@@ -6,14 +7,15 @@ namespace CourseWork.Models
     public class Book
     {
         public long Id { get; set; }
-        public int NumberOfPages { get; set; }
-        public string Name { get; set; } = "";
+        public int NumberOfPages { get; set; } = 0;
+        [Required]
+        public string? Name { get; set; }
         public string Publisher { get; set; } = "";
         public string Isbn10 { get; set; } = "";
         public string Isbn13 { get; set; } = "";
         public IBitmap? Cover { get; set; }
         public bool IsPresent { get; set; } = true;
-        public int PublishingYear { get; set; }
+        public int PublishingYear { get; set; } = 0;
         public IList<Author> Authors { get; set; } = new List<Author>();
         public IList<Subject> Subjects { get; set; } = new List<Subject>();
     }
