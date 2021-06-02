4f1a5c78e3635da45dbd729d9a3127a50e3c5081
max <mail.max.khomenko@gmail.com>
2021-06-02 19:25:56 +0300

    Added tests for database

---
+++ CourseWork.Tests/DatabaseTests.cs
@@ -0,0 +1,53 @@
+using System.Data.Common;
+using CourseWork.Database;
+using CourseWork.Models;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+
+namespace CourseWork.Tests
+{
+    public class InMemoryBookContext : BookContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite(GetInMemoryConnection());
+        }
+
+        private static DbConnection GetInMemoryConnection()
+        {
+            return new SqliteConnection("Filename=:memory:");
+        }
+    }
+
+    public class DatabaseTests
+    {
+        private BookContext _context;
+
+        [SetUp]
+        public void Setup()
+        {
+            _context = new BookContext();
+        }
+
+        [Test]
+        public void BookModelCreate_AddingBitmap_HandlesNullability()
+        {
+            var book = new Book
+            {
+                Name = "",
+                Cover = null
+            };
+            _context.Add(book);
+            Assert.That(_context.SaveChanges, Throws.Nothing);
+        }
+
+        [Test]
+        public void BookModelCreate_AddingNewName_ForbidsNull()
+        {
+            var book = new Book(); // "name" left uninitialized
+            _context.Add(book);
+            Assert.That(_context.SaveChanges, Throws.Exception.InstanceOf<DbUpdateException>());
+        }
+    }
+}
