4411bad094738109ffac6b02560ca6b1730427de
max <mail.max.khomenko@gmail.com>
2021-06-06 17:55:26 +0300

    Added validation for ISBN code

---
+++ CourseWork/ViewModels/AddBookScreenViewModel.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+using ReactiveUI;
+
+namespace CourseWork.ViewModels
+{
+    public class AddBookScreenViewModel : ViewModelBase, INotifyDataErrorInfo
+    {
+        private static readonly Regex IsbnRegex = new(@"^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d\re-]+$");
+
+        private string _isbn = "";
+        private string? _isbnValidationError;
+
+        public AddBookScreenViewModel()
+        {
+            this.WhenAnyValue(o => o.Isbn).Subscribe(_ => ValidateAll());
+        }
+
+        public string Isbn
+        {
+            get => _isbn;
+            set => this.RaiseAndSetIfChanged(ref _isbn, value);
+        }
+
+        public event EventHandler<DataErrorsChangedEventArgs>? ErrorsChanged;
+
+        public IEnumerable GetErrors(string? propertyName)
+        {
+            yield return propertyName switch
+            {
+                nameof(Isbn) => _isbnValidationError!,
+                _ => Enumerable.Empty<string>()
+            };
+        }
+
+        public bool HasErrors => _isbnValidationError != null;
+
+        private void ValidateAll()
+        {
+            if (_isbn.Length is 10 or 13 && IsbnRegex.IsMatch(_isbn))
+            {
+                if (_isbnValidationError == null) return;
+                _isbnValidationError = null;
+                ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(Isbn)));
+            }
+            else
+            {
+                if (_isbnValidationError != null) return;
+                _isbnValidationError = "Invalid ISBN";
+                ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(Isbn)));
+            }
+        }
+    }
+}
