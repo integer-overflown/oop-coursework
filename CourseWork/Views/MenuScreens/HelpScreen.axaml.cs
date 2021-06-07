d8112399e97c2198b1edd3b354ae7da6f091f8b7
max <mail.max.khomenko@gmail.com>
2021-06-07 19:23:04 +0300

    Added dummy help screen

---
+++ CourseWork/Views/MenuScreens/HelpScreen.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CourseWork.Views.MenuScreens
+{
+    public class HelpScreen : UserControl
+    {
+        public HelpScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
