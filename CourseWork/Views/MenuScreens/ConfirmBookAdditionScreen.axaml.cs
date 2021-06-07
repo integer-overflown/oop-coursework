4dd660f667a95947e471f480c60770ee7e72d672
max <mail.max.khomenko@gmail.com>
2021-06-07 04:33:36 +0300

    Added a header for book addition screen

---
+++ CourseWork/Views/MenuScreens/ConfirmBookAdditionScreen.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace CourseWork.Views.MenuScreens
+{
+    public class ConfirmBookAdditionScreen : UserControl
+    {
+        public ConfirmBookAdditionScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
