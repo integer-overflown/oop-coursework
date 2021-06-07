0963a2893b5862f90c58470a338be14adf73bd79
max <mail.max.khomenko@gmail.com>
2021-06-07 19:36:01 +0300

    Added basic navigation logic

--- CourseWork/Views/MainWindow.axaml.cs
+++ CourseWork/Views/MainWindow.axaml.cs
@@ -3,15 +3,20 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using CourseWork.Models;
 using CourseWork.ViewModels;
+using CourseWork.Views.MenuScreens;
 
 namespace CourseWork.Views
 {
     public partial class MainWindow : Window
     {
+        private readonly Carousel _screens;
+
         public MainWindow()
         {
             InitializeComponent();
+            _screens = this.FindControlStrict<Carousel>("Pages");
 #if DEBUG
             this.AttachDevTools();
 #endif
@@ -20,13 +25,12 @@ namespace CourseWork.Views
 
         private void SetupPageChangeListeners()
         {
-            var pages = this.FindControlStrict<Carousel>("Pages");
             if (DataContext is MainWindowViewModel viewModel)
             {
                 viewModel.Changed.Subscribe(args =>
                 {
                     if (args.PropertyName != nameof(viewModel.CurrentScreenIndex)) return;
-                    pages.SelectedIndex = viewModel.CurrentScreenIndex;
+                    _screens.SelectedIndex = viewModel.CurrentScreenIndex;
                 });
             }
         }
@@ -47,6 +51,18 @@ namespace CourseWork.Views
             {
                 control.PointerPressed += (_, _) => { viewModel.CurrentScreenIndex = index; };
             }
+        }
+
+        private void SearchBookByIsbnScreen_OnSearchSucceeded(ISearchAgent<Book>.SearchSucceededEventArgs args)
+        {
+            if (args.Result == null) return;
+            var screen = _screens.FindControlStrict<BookViewScreen>("BookViewScreen");
+            screen.Book = args.Result;
+        }
+
+        private void SearchBookByIsbnScreen_OnSearchFailed(ISearchAgent<Book>.SearchFailedEventArgs args)
+        {
+            Console.WriteLine(args.Reason);
         }
     }
 }
