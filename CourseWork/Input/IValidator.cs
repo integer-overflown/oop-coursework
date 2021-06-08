23082ee14d71a5977fbb7a6ca6bd8e9994740fd2
max <mail.max.khomenko@gmail.com>
2021-06-08 14:51:35 +0300

    Added digits validator

--- CourseWork/Input/IValidator.cs
+++ CourseWork/Input/IValidator.cs
@@ -3,5 +3,6 @@ namespace CourseWork.Input
     public interface IValidator<in T>
     {
         bool IsValid(T value);
+        bool IsPermitted(T value);
     }
 }
--- CourseWork/Input/Validators.cs
+++ CourseWork/Input/Validators.cs
@@ -6,16 +6,36 @@ namespace CourseWork.Input
     public static class Validators
     {
         public static IValidator<string> Isbn => new IsbnValidator();
+        public static IValidator<string> Digits => new DigitOnlyStringValidator();
     }
 
     public class IsbnValidator : IValidator<string>
     {
         private static readonly Regex IsbnRegex = new(@"^(?=(?:\D*\d){10}(?:(?:\D*\d){3})?$)[\d\re\-]+$");
+        private static readonly Regex SequenceRegex = new(@"[0-9\-]*");
 
         public bool IsValid(string value)
         {
             var digits = value.Count(char.IsDigit);
             return digits is 10 or 13 && IsbnRegex.IsMatch(value);
+        }
+
+        public bool IsPermitted(string value)
+        {
+            return SequenceRegex.IsMatch(value);
+        }
+    }
+
+    public class DigitOnlyStringValidator : IValidator<string>
+    {
+        public bool IsValid(string value)
+        {
+            return value.All(char.IsDigit);
+        }
+
+        public bool IsPermitted(string value)
+        {
+            return IsValid(value);
         }
     }
 }
