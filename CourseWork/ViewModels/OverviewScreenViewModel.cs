4de4acf2ebbe391fe93590b6e490614c748d6fe1
max <mail.max.khomenko@gmail.com>
2021-06-10 23:42:19 +0300

    Added wrapper item for displaying books

--- CourseWork/ViewModels/OverviewScreenViewModel.cs
+++ CourseWork/ViewModels/OverviewScreenViewModel.cs
@@ -1,4 +1,5 @@
 using System.Collections.ObjectModel;
+using System.Linq;
 using CourseWork.Models;
 using ReactiveUI;
 
@@ -6,21 +7,38 @@ namespace CourseWork.ViewModels
 {
     public class OverviewScreenViewModel : ViewModelBase
     {
-        private ObservableCollection<Book> _books = new();
+        private ObservableCollection<BookDisplayItem> _books = new();
 
         public OverviewScreenViewModel()
         {
             _books.Add(new Book
             {
-                Authors = new []{new Author {Name = "Erich Maria Remarque"}},
+                Authors = new[] {new Author {Name = "Erich Maria Remarque"}},
                 Name = "Three Comrades"
             });
         }
-        
-        public ObservableCollection<Book> Books
+
+        public ObservableCollection<BookDisplayItem> Books
         {
             get => _books;
             set => this.RaiseAndSetIfChanged(ref _books, value, nameof(Books));
+        }
+
+        public readonly struct BookDisplayItem
+        {
+            public Book Content { get; }
+            public string? FirstAuthorName { get; }
+
+            public static implicit operator BookDisplayItem(Book book)
+            {
+                return new(book);
+            }
+
+            private BookDisplayItem(Book book)
+            {
+                Content = book;
+                FirstAuthorName = book.Authors.FirstOrDefault()?.Name;
+            }
         }
     }
 }
