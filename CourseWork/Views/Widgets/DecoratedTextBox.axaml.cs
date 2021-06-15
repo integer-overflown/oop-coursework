75262f4c59057aa1edf9bb06315b9842d3ad3a19
max <mail.max.khomenko@gmail.com>
2021-06-15 15:24:42 +0300

    Bugfix: proper bindings

--- CourseWork/Views/Widgets/DecoratedTextBox.axaml.cs
+++ CourseWork/Views/Widgets/DecoratedTextBox.axaml.cs
@@ -1,20 +1,65 @@
 using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Data;
+using Avalonia.Media;
 
 namespace CourseWork.Views.Widgets
 {
-    public class DecoratedTextBox : IllustratedTextControlTemplate
+    public class DecoratedTextBox : TemplatedControl
     {
         private const int DefaultIconSize = 24;
 
-        public static readonly AvaloniaProperty<string?> WatermarkProperty =
-            AvaloniaProperty.RegisterDirect<DecoratedTextBox, string?>(nameof(Watermark), o => o.Watermark,
-                (o, v) => o.Watermark = v);
+        public static readonly StyledProperty<string?> WatermarkProperty =
+            TextBox.WatermarkProperty.AddOwner<DecoratedTextBox>();
+
+        public static readonly DirectProperty<DecoratedTextBox, string?> TextProperty =
+            AvaloniaProperty.RegisterDirect<DecoratedTextBox, string?>(nameof(Text), o => o.Text, (o, v) => o.Text = v,
+                defaultBindingMode: BindingMode.TwoWay);
+
+        public static readonly AvaloniaProperty<IImage?> IconSourceProperty =
+            AvaloniaProperty.RegisterDirect<DecoratedTextBox, IImage?>(nameof(IconSource),
+                o => o.IconSource,
+                (o, v) => o.IconSource = v);
+
+        public static readonly AvaloniaProperty<int> IconSizeProperty =
+            AvaloniaProperty.RegisterDirect<DecoratedTextBox, int>(nameof(IconSource), o => o.IconSize,
+                (o, v) => o.IconSize = v);
+
+        private int _iconSize;
+
+        private IImage? _iconSource;
+
+        private string? _text;
+
 
         public DecoratedTextBox()
         {
             IconSize = DefaultIconSize;
         }
 
-        public string? Watermark { get; set; }
+        public string? Watermark
+        {
+            get => GetValue(WatermarkProperty);
+            set => SetValue(WatermarkProperty, value);
+        }
+
+        public string? Text
+        {
+            get => _text;
+            set => SetAndRaise(TextProperty, ref _text, value);
+        }
+
+        public IImage? IconSource
+        {
+            get => _iconSource;
+            set => SetAndRaise(IconSourceProperty, ref _iconSource, value);
+        }
+
+        public int IconSize
+        {
+            get => _iconSize;
+            set => SetAndRaise(IconSizeProperty, ref _iconSize, value);
+        }
     }
 }
