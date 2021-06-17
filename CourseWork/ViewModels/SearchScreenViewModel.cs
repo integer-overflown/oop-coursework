f2a2de44879989192daa514d1bb5fd0f0b75e762
max <mail.max.khomenko@gmail.com>
2021-06-17 05:47:11 +0300

    Added basic search screen logic

---
+++ CourseWork/ViewModels/SearchScreenViewModel.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Linq;
+using CourseWork.Models;
+
+namespace CourseWork.ViewModels
+{
+    public class SearchScreenViewModel : ViewModelBase
+    {
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public string? Publisher { get; set; }
+        public string? Subject { get; set; }
+
+        public Func<Book, bool> CreateFilter()
+        {
+            return book => AuthorFilter(book, Author)
+                           && PublisherFilter(book, Publisher)
+                           && SubjectFilter(book, Subject);
+        }
+
+        private static bool NotNullAndContains(string? s, string value)
+        {
+            return s != null && s.Contains(value, StringComparison.OrdinalIgnoreCase);
+        }
+
+        private static bool AuthorFilter(Book book, string? author)
+        {
+            return string.IsNullOrWhiteSpace(author) || book.Authors
+                .Select(a => a.Name)
+                .Any(n => NotNullAndContains(n, author));
+        }
+
+        private static bool SubjectFilter(Book book, string? subject)
+        {
+            return string.IsNullOrWhiteSpace(subject) || book.Subjects
+                .Select(a => a.Name)
+                .Any(n => NotNullAndContains(n, subject));
+        }
+
+        private static bool PublisherFilter(Book book, string? publisher)
+        {
+            return string.IsNullOrWhiteSpace(publisher) || NotNullAndContains(book.Publisher, publisher);
+        }
+
+        private Func<Book, bool> GetFilter(string property, string? param)
+        {
+            return property switch
+            {
+                nameof(Author) => book => AuthorFilter(book, param),
+                nameof(Publisher) => book => PublisherFilter(book, param),
+                nameof(Subject) => book => SubjectFilter(book, param),
+                _ => throw new ArgumentException("Failed to match filter to property.", nameof(property))
+            };
+        }
+    }
+}
